ceb2406f3d3ce807871a6e1255a09825ca64686b
SPGoding <SPGoding@outlook.com>
2019-07-06 21:52:30 +0800

    :sparkles: Add definition for models

--- src/utils/Model.ts
+++ src/utils/Model.ts
@@ -2,54 +2,56 @@
  * Structure of a model file.
  */
 export default interface Model {
-    parent: string,
-    ambientocclusion: boolean,
-    textures: {
+    parent?: string,
+    ambientocclusion?: boolean,
+    textures?: {
         [variable: string]: string
     },
-    display: {
-        thirdperson_righthand: Display,
-        thirdperson_lefthand: Display,
-        firstperson_righthand: Display,
-        firstperson_lefthand: Display,
-        gui: Display,
-        head: Display,
-        ground: Display,
-        fixed: Display,
+    display?: {
+        thirdperson_righthand?: Display,
+        thirdperson_lefthand?: Display,
+        firstperson_righthand?: Display,
+        firstperson_lefthand?: Display,
+        gui?: Display,
+        head?: Display,
+        ground?: Display,
+        fixed?: Display
     },
-    elements: Element[]
+    elements?: Element[],
+    [other: string]: any
 }
 
 type Display = {
-    rotation: number[],
-    scale: number[],
-    translation: number[]
+    rotation?: number[],
+    scale?: number[],
+    translation?: number[]
 }
 
 type Element = {
-    from: number[],
-    to: number[],
-    rotation: {
-        origin: number[],
-        axis: 'x' | 'y' | 'z',
-        angle: number,
-        rescale: boolean
+    from?: number[],
+    to?: number[],
+    rotation?: {
+        origin?: number[],
+        axis?: 'x' | 'y' | 'z',
+        angle?: number,
+        rescale?: boolean
     },
-    shade: boolean,
-    faces: {
-        down: Face,
-        up: Face,
-        north: Face,
-        south: Face,
-        west: Face,
-        east: Face
-    }
+    shade?: boolean,
+    faces?: {
+        down?: Face,
+        up?: Face,
+        north?: Face,
+        south?: Face,
+        west?: Face,
+        east?: Face
+    },
+    [other: string]: any
 }
 
 type Face = {
-    uv: number[],
-    texture: string,
-    cullface: 'down' | 'up' | 'north' | 'south' | 'west' | 'east',
-    rotation: number,
-    tintindex: number
+    uv?: number[],
+    texture?: string,
+    cullface?: 'down' | 'up' | 'north' | 'south' | 'west' | 'east',
+    rotation?: number,
+    tintindex?: number
 }
